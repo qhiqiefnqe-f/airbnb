a6470aed6fb04a1355a6910cf1ce9fd175484871
qhiqiefnqe-f <739726896@qq.com>
2025-03-17 20:02:35 +0800

    store基本完成

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,11 +1,81 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { getHomeDiscountData, getHomeGoodPriceData, getHomeHighScoreData, getHomeHotRecommendData, getHomeLongforData, getHomePlusData } from '@/services'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+export const fetchHomeDataAction = createAsyncThunk("fetchdata", async (payload, { dispatch }) => {
+  getHomeGoodPriceData().then(res => {
+    dispatch(changeGoodPriceInfoAction(res))
+  })
+  getHomeHighScoreData().then(res => {
+    dispatch(changeHighScoreInfoAction(res))
+  })
+  getHomeDiscountData().then(res => {
+    dispatch(changeDiscountInfoAction(res))
+  })
+  getHomeHotRecommendData().then(res => {
+    dispatch(changeRecommendInfoAction(res))
+  })
+  getHomeLongforData().then(res => {
+    dispatch(changeLongforInfoAction(res))
+  })
+  getHomePlusData().then(res => {
+    dispatch(changePlusInfoAction(res))
+  })
+})
+
 
 const homeSlice = createSlice({
   name: "home",
   initialState: {
-    name: "home",
+    goodPriceInfo: {},
+    highScoreInfo: {},
+    discountInfo: {},
+    recommendInfo: {},
+    longforInfo: {},
+    plusInfo: {}
   },
-  reducers: {},
-});
+  reducers: {
+    changeGoodPriceInfoAction(state, { payload }) {
+      state.goodPriceInfo = payload
+    },
+    changeHighScoreInfoAction(state, { payload }) {
+      state.highScoreInfo = payload
+    },
+    changeDiscountInfoAction(state, { payload }) {
+      state.discountInfo = payload
+    },
+    changeRecommendInfoAction(state, { payload }) {
+      state.recommendInfo = payload
+    },
+    changeLongforInfoAction(state, { payload }) {
+      state.longforInfo = payload
+    },
+    changePlusInfoAction(state, { payload }) {
+      state.plusInfo = payload
+    }
+  },
+  // 修改为 builder 回调形式
+  extraReducers: (builder) => {
+    // 如果需要处理 fetchHomeDataAction 的状态，可以添加以下代码
+    // builder
+    //   .addCase(fetchHomeDataAction.fulfilled, (state, { payload }) => {
+    //     // 处理成功状态
+    //   })
+    //   .addCase(fetchHomeDataAction.pending, (state) => {
+    //     // 处理加载状态
+    //   })
+    //   .addCase(fetchHomeDataAction.rejected, (state, action) => {
+    //     // 处理错误状态
+    //   });
+  }
+})
 
-export default homeSlice.reducer;
+export const {
+  changeGoodPriceInfoAction,
+  changeHighScoreInfoAction,
+  changeDiscountInfoAction,
+  changeRecommendInfoAction,
+  changeLongforInfoAction,
+  changePlusInfoAction
+} = homeSlice.actions
+
+export default homeSlice.reducer
